51505262136057419753e9a7ad5592f8a2a952c7
Sven Lüchow <sven.luechow@icloud.com>
2022-03-03 15:46:32 +0100

    Change Style on App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-
 import Header from "./components/Header";
+import List from "./components/List";
 
 function App() {
   return (
     <>
       <Header />
+      <List className="List" />
     </>
   );
 }
