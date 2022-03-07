13a1ab55251010fecaab0097e47976570c0ddab1
Marie-Carleen Frei <lene@7freiheiten.de>
2022-03-07 10:40:13 +0100

    Remove filter from return statement

--- src/components/SearchListItems.js
+++ src/components/SearchListItems.js
@@ -1,24 +1,27 @@
 import "./searchlistitems.css";
 
-export default function SearchListItems({ searchInput, items, onAddSearchedItem }) {
+export default function SearchListItems({
+  searchInput,
+  items,
+  onAddSearchedItem,
+}) {
+  const filterdItems = items.filter((item) =>
+    item.name.en.toLowerCase().includes(searchInput.toLowerCase())
+  );
+
   return (
     <ul className="search-list">
-      {items
-        .filter((item) => {
-          return item.name.en.toLowerCase().includes(searchInput.toLowerCase());
-        })
-
-        .map((item) => (
-          <li
-            className="search-list-item"
-            key={item._id}
-            onClick={() => {
-              onAddSearchedItem(item);
-            }}
-          >
-            {item.name.en}
-          </li>
-        ))}
+      {filterdItems.map((item) => (
+        <li
+          className="search-list-item"
+          key={item._id}
+          onClick={() => {
+            onAddSearchedItem(item);
+          }}
+        >
+          {item.name.en}
+        </li>
+      ))}
     </ul>
   );
 }
